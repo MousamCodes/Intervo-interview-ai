9e6d907837eb2433eff7469d2ee471152ea5d2ab
MousamCodes <mousampodder02@gmail.com>
2026-04-15 13:22:40 +0530

    fixed

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -4,15 +4,16 @@ const Page = async () => {
 
   const user = await getCurrentUser();
   return (
-    <>
-        <h3>Interview Generation</h3>
-
-        <Agent
-        userName={user?.name!}
-        userId={user?.id}
-        type="generate"
-      />
-    </>
+    <div>
+      <h3>Interview Generation</h3>
+      {user && (
+        <Agent 
+          userName={user.name} 
+          userId={user.id} 
+          type="generate" 
+        />
+      )}
+    </div>
   )
 }
 
